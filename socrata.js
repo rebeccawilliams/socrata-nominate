1a795e448741dd7c46265aa1c6efcf30d689abfa
Thomas Levine <occurrence@thomaslevine.com>
2013-08-04 12:03:30 -0700

    not commonjs

--- socrata.js
+++ socrata.js
@@ -1,32 +1,43 @@
-(function(){
-  module.exports = {}
+var socrata = (function(){
+  var webpage = require('webpage')
+  var socrata = {}
 
-  module.exports.is_domain = function(potential_url){
+  socrata.is_domain = function(potential_url){
     return (null !== potential_url.match(/\./)) && (null == potential_url.match(/ /))
   }
 
-  module.exports.sites = function() {
-def sites():
-    handle = urlopen('http://status.socrata.com/sites')
-    portals = json.load(handle)
-    for portal in portals:
-        if is_domain(portal['description']):
-            domain = portal['description']
-        elif is_domain(portal['name']):
-            domain = portal['name']
-        elif portal['name'] == 'Socrata':
-            continue
-        else:
-            warnings.warn('Could not find a valid domain for %s, skipping' % portal['name'])
-            continue
-
-        domain = domain.replace('https://', '').replace('http://', '')
-        yield domain
-  }
-  module.exports.nominate = function(site, title, description) {
-    var page = require('webpage').create()
-    page.open(url, function (status) {
-      phantom.exit();
+  socrata.sites = function() {
+    var page = webpage.create()
+    page.open('http://status.socrata.com/sites', function(status) {
+      console.log(page.plainText)
     })
   }
-})
+      /*
+      portals = json.load(handle)
+      for portal in portals:
+          if is_domain(portal['description']):
+              domain = portal['description']
+          elif is_domain(portal['name']):
+              domain = portal['name']
+          elif portal['name'] == 'Socrata':
+              continue
+          else:
+              warnings.warn('Could not find a valid domain for %s, skipping' % portal['name'])
+              continue
+
+          domain = domain.replace('https://', '').replace('http://', '')
+          yield domain
+      */
+
+  /*
+  socrata.nominate = function(site, title, description) {
+    var page = require('webpage').create()
+    page.open(url, function (status) {
+      phantom.exit()
+    })
+  }
+  */
+  return socrata
+})()
+
+socrata.sites()
